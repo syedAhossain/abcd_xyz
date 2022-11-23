dec838887fe0f2b1183f5bd335a65c913cf84915
syedAhossain <118427502+syedAhossain@users.noreply.github.com>
2022-11-23 17:30:37 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,640 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script src="https://cdn.tailwindcss.com"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="https://fonts.maateen.me/solaiman-lipi/font.css" rel="stylesheet">
+		
+		<title>Document</title>
+	</head>
+	<body>
+		<div class="container p-4">
+			<div class="columns-2 font-serif point-table">
+				<div class="bg-slate-100/75 text-xs text-slate-800 p-5 todays-match">
+					<div class="columns-2">
+						<p class="p-1 rounded-t-md tracking-wider group-name">Group Name</p>
+						<p class="text-right rounded-b-md p-1 game-date">Date</p>
+					</div>
+					<p class="p-1 venue-name">Venue</p>
+					<div class="columns-2 flex justify-between">
+						<div class="game-teams">
+							<p class="p-1 team-name"><span class="team-flag"></span>ARG</p>
+							<p class="p-1 team-name"><span class="team-flag"></span>BRA</p>
+						</div>
+						<div class="flex items-center game-time">
+							Time
+						</div>
+					</div>
+				</div>
+
+				<div class="bg-slate-100/75 text-xs text-slate-800 p-5 todays-match">
+					<div class="columns-2">
+						<p class="p-1 rounded-t-md tracking-wider group-name">Group Name</p>
+						<p class="text-right rounded-b-md p-1 game-date">Date</p>
+					</div>
+					<p class="p-1 venue-name">Venue</p>
+					<div class="columns-2 flex justify-between">
+						<div class="game-teams">
+							<p class="p-1 team-name"><span class="team-flag"></span>ARG</p>
+							<p class="p-1 team-name"><span class="team-flag"></span>BRA</p>
+						</div>
+						<div class="flex items-center game-time">
+							Time
+						</div>
+					</div>
+				</div>
+
+				<div class="bg-slate-100/75 text-xs text-slate-800 p-5 todays-match">
+					<div class="columns-2">
+						<p class="p-1 rounded-t-md tracking-wider group-name">Group Name</p>
+						<p class="text-right rounded-b-md p-1 game-date">Date</p>
+					</div>
+					<p class="p-1 venue-name">Venue</p>
+					<div class="columns-2 flex justify-between">
+						<div class="game-teams">
+							<p class="p-1 team-name"><span class="team-flag"></span>ARG</p>
+							<p class="p-1 team-name"><span class="team-flag"></span>BRA</p>
+						</div>
+						<div class="flex items-center game-time">
+							Time
+						</div>
+					</div>
+				</div>
+
+				<div class="bg-slate-100/75 text-xs text-slate-800 p-5 todays-match">
+					<div class="columns-2">
+						<p class="p-1 rounded-t-md tracking-wider group-name">Group Name</p>
+						<p class="text-right rounded-b-md p-1 game-date">Date</p>
+					</div>
+					<p class="p-1 venue-name">Venue</p>
+					<div class="columns-2 flex justify-between">
+						<div class="game-teams">
+							<p class="p-1 team-name"><span class="team-flag"></span>ARG</p>
+							<p class="p-1 team-name"><span class="team-flag"></span>BRA</p>
+						</div>
+						<div class="flex items-center game-time">
+							Time
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="point-table">
+				<header>
+					<h1>বিশ্বকাপ ফুটবল পয়েন্ট টেবিল</h1>
+				</header>
+
+		  		<section class="text-xs">
+				    <div class="columns-2 gap-5">
+				    	<div class="bg-slate-100 w-full p-4 rounded-md m4-4 mb-4 group-tables">
+							<h2 class="text-lg font-semibold mb-2">গ্রুপ এ</h2>
+					    	<table class="table-fixed w-full bg-zinc-50 p-2 rounded-md">
+								<thead>
+									<tr class="title">
+										<th colspan=2>দল</th>
+										<th>ম্যাচ</th>
+										<th>জয়</th>
+										<th>ড্র</th>
+										<!-- <th>পক্ষে</th>
+										<th>বিপক্ষে</th> -->
+										<th>ব্যবধান</th>
+										<th>পয়েন্ট</th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr>
+										<td colspan=2>কাতার</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>সেনেগাল</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>নেদারল্যান্ডস</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>ইকুয়েডর</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+								</tbody>
+						    </table>
+					    </div>
+
+					    <div class="bg-slate-100 w-full p-4 rounded-md m4-4 mb-4 group-tables">
+							<h2 class="text-lg font-semibold mb-2">গ্রুপ বি</h2>
+						    <table class="table-fixed w-full bg-zinc-50 p-2 rounded-md">
+								<thead>
+									<tr class="title">
+										<th colspan=2>দল</th>
+										<th>ম্যাচ</th>
+										<th>জয়</th>
+										<th>ড্র</th>
+										<!-- <th>পক্ষে</th>
+										<th>বিপক্ষে</th> -->
+										<th>ব্যবধান</th>
+										<th>পয়েন্ট</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan=2>ইংল্যান্ড</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>যুক্তরাষ্ট্র</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>ওয়েলস</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>ইরান</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+								</tbody>
+						  	</table>
+					  	</div>
+				    </div>
+				  	
+				  	<div class="columns-2 gap-5">
+				  		<div class="bg-slate-100 w-full p-4 rounded-md m4-4 mb-4 group-tables">
+					  		<h2 class="text-lg font-semibold mb-2">গ্রুপ সি</h2>
+							<table class="table-fixed w-full bg-zinc-50 p-2 rounded-md p-2 rounded-md">
+								<thead>
+									<tr class="title">
+										<th colspan=2>দল</th>
+										<th>ম্যাচ</th>
+										<th>জয়</th>
+										<th>ড্র</th>
+										<!-- <th>পক্ষে</th>
+										<th>বিপক্ষে</th> -->
+										<th>ব্যবধান</th>
+										<th>পয়েন্ট</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan=2>আর্জেন্টিনা</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>মেক্সিকো</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>পোল্যান্ড</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>সৌদি আরব</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+
+						<div class="bg-slate-100 w-full p-4 rounded-md m4-4 mb-4 group-tables">
+							<h2 class="text-lg font-semibold mb-2">গ্রুপ ডি</h2>
+							<table class="table-fixed w-full bg-zinc-50 p-2 rounded-md">
+								<thead>
+									<tr class="title">
+										<th colspan=2>দল</th>
+										<th>ম্যাচ</th>
+										<th>জয়</th>
+										<th>ড্র</th>
+										<!-- <th>পক্ষে</th>
+										<th>বিপক্ষে</th> -->
+										<th>ব্যবধান</th>
+										<th>পয়েন্ট</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan=2>ডেনমার্ক</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>ফ্রান্স</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>তিউনিসিয়া</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>অস্ট্রেলিয়া</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+				  	</div>
+
+					<div class="columns-2 gap-5">
+						<div class="bg-slate-100 w-full p-4 rounded-md m4-4 mb-4 group-tables">
+							<h2 class="text-lg font-semibold mb-2">গ্রুপ ই</h2>
+							<table class="table-fixed w-full bg-zinc-50 p-2 rounded-md">
+								<thead>
+									<tr class="title">
+										<th colspan=2>দল</th>
+										<th>ম্যাচ</th>
+										<th>জয়</th>
+										<th>ড্র</th>
+										<!-- <th>পক্ষে</th>
+										<th>বিপক্ষে</th> -->
+										<th>ব্যবধান</th>
+										<th>পয়েন্ট</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan=2>জার্মানি</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>স্পেন</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>জাপান</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>কোস্টারিকা</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+
+						<div class="bg-slate-100 w-full p-4 rounded-md m4-4 mb-4 group-tables">
+							<h2 class="text-lg font-semibold mb-2">গ্রুপ এফ</h2>
+							<table class="table-fixed w-full bg-zinc-50 p-2 rounded-md">
+								<thead>
+									<tr class="title">
+										<th colspan=2>দল</th>
+										<th>ম্যাচ</th>
+										<th>জয়</th>
+										<th>ড্র</th>
+										<!-- <th>পক্ষে</th>
+										<th>বিপক্ষে</th> -->
+										<th>ব্যবধান</th>
+										<th>পয়েন্ট</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan=2>মরক্কো</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>বেলজিয়াম</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>ক্রোয়েশিয়া</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>কানাডা</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+
+					<div class="columns-2 gap-5">
+						<div class="bg-slate-100 w-full p-4 rounded-md m4-4 mb-4 group-tables">
+							<h2 class="text-lg font-semibold mb-2">গ্রুপ জি</h2>
+							<table class="table-fixed w-full bg-zinc-50 p-2 rounded-md">
+								<thead>
+									<tr class="title">
+										<th colspan=2>দল</th>
+										<th>ম্যাচ</th>
+										<th>জয়</th>
+										<th>ড্র</th>
+										<!-- <th>পক্ষে</th>
+										<th>বিপক্ষে</th> -->
+										<th>ব্যবধান</th>
+										<th>পয়েন্ট</th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr>
+										<td colspan=2>সুইজারল্যান্ড</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>ব্রাজিল</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+									<!-- 	<td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>ক্যামেরুন</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>সার্বিয়া</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+
+						<div class="bg-slate-100 w-full p-4 rounded-md m4-4 mb-4 group-tables">
+							<h2 class="text-lg font-semibold mb-2">গ্রুপ এইচ</h2>
+							<table class="table-fixed w-full bg-zinc-50 p-2 rounded-md">
+								<thead>
+									<tr class="title">
+										<th colspan=2>দল</th>
+										<th>ম্যাচ</th>
+										<th>জয়</th>
+										<th>ড্র</th>
+										<!-- <th>পক্ষে</th>
+										<th>বিপক্ষে</th> -->
+										<th>ব্যবধান</th>
+										<th>পয়েন্ট</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan=2>উরুগুয়ে</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>পর্তুগাল</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>দ. কোরিয়া</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td colspan=2>ঘানা</td>
+										<td>0</td>
+										<td>0</td>
+										<td>0</td>
+										<!-- <td>0</td>
+										<td>0</td> -->
+										<td>0</td>
+										<td>0</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</section>
+			</div>
+
+			<!-- <div class="table w-full ...">
+			  <div class="table-header-group ...">
+			    <div class="table-row">
+			      <div class="table-cell text-left ...">Song</div>
+			      <div class="table-cell text-left ...">Artist</div>
+			      <div class="table-cell text-left ...">Year</div>
+			    </div>
+			  </div>
+			  <div class="table-row-group">
+			    <div class="table-row">
+			      <div class="table-cell ...">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+			      <div class="table-cell ...">Malcolm Lockyer</div>
+			      <div class="table-cell ...">1961</div>
+			    </div>
+			    <div class="table-row">
+			      <div class="table-cell ...">Witchy Woman</div>
+			      <div class="table-cell ...">The Eagles</div>
+			      <div class="table-cell ...">1972</div>
+			    </div>
+			    <div class="table-row">
+			      <div class="table-cell ...">Shining Star</div>
+			      <div class="table-cell ...">Earth, Wind, and Fire</div>
+			      <div class="table-cell ...">1975</div>
+			    </div>
+			  </div>
+			</div> -->
+		</div>
+
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+		<script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript">
+          const options = { 
+            method: 'GET', 
+            headers: new Headers({
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzdkZWFlYTU3MDgxNjZkMDdiMjBiMjAiLCJpYXQiOjE2NjkxOTY2MjMsImV4cCI6MTY2OTI4MzAyM30.Bp0Tp_-EWPzuLjNCgO_McVJcEL23leVFk1hLdFfFHD8',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'
+            })
+          }
+
+          const proxyUrl = "https://thingproxy.freeboard.io/fetch/";
+          // const proxyUrl = "http://alloworigin.com/get?url=";
+
+          // const urlMaker = `https://api.allorigins.win/get?url=http://api.cup2022.ir/api/v1/team/6}`
+          const teamIdUrl = 'http://api.cup2022.ir/api/v1/team';
+          fetch(proxyUrl+teamIdUrl, options)
+                .then(function(res) {
+                  console.log(res.json());
+                  return res.json();
+                 })
+                // .then(function(resJson) {
+                //   console.log(resJson);
+                //  })
+    
+    </script>
+	</body>
+</html>
